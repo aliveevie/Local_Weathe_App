cb40d42273d53d2b0d0ce866507cc4946848e7cb
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-07-27 13:35:29 +0100

    modified data.js

--- data.js
+++ data.js
@@ -4,4 +4,4 @@ const longitude = 3.3792057;
 
 fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`)
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => console.log(data.coord.lon));
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-
 function App() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [data, setData] = useState(null);
-  
+  const [isCelsius, setIsCelsius] = useState(true);
+  const [temperature, setTemperature] = useState(null);
+
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
@@ -21,7 +22,6 @@ function App() {
     } else {
       console.error("Geolocation is not available in this browser.");
     }
-    
   }, []);
 
   useEffect(() => {
@@ -29,21 +29,51 @@ function App() {
       fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`)
         .then(response => response.json())
         .then(data => setData(data))
+        .catch(error => {
+          console.error("Error fetching weather data:", error);
+        });
     }
   }, [latitude, longitude]);
 
+  useEffect(() => {
+    // Assuming you have the temperature data in Celsius
+    if (data && data.main && data.main.temp) {
+      setTemperature(data.main.temp);
+    }
+  }, [data]);
+
+  const celsiusToFahrenheit = (celsius) => {
+    const fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+  };
+
+  const handleClick = () => {
+    setIsCelsius(prevState => !prevState);
+    if (isCelsius) {
+      setTemperature(celsiusToFahrenheit(temperature));
+    } else {
+      setTemperature(data.main.temp);
+    }
+  };
   
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1>FreeCode Camp <br></br>Weather App</h1>
-      <p>{data.name}, {data.sys.country}</p>
-      <p>{data.main.temp} °C</p>
-      <p>{data.weather[0].main}</p>
-      <img src={data.weather[0].icon} alt='Wheather Icon' />
-         
-          
-
+        <h1>FreeCode Camp <br />Weather App</h1>
+        {data && data.name && data.sys.country && (
+          <p>{data.name}, {data.sys.country}</p>
+        )}
+        {temperature !== null && (
+          <p>{temperature} {isCelsius ? "°C" : "°F"}</p>
+        )}
+        {data && data.weather && data.weather.length > 0 && (
+          <div>
+            <p>{data.weather[0].main}</p>
+            <img src={data.weather[0].icon} alt='Weather Icon' />
+          </div>
+        )}
+        <button onClick={handleClick}>Change temp</button>   
       </header>
     </div>
   );
