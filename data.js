93f6a1e9bcadcd5090ab939c01768d2571fff381
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-07-27 11:59:44 +0100

    fix: modified the src/js

---
+++ data.js
@@ -0,0 +1,7 @@
+const latitude = 6.5243793;
+
+const longitude = 3.3792057;
+
+fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`)
+.then(response => response.json())
+.then(data => console.log(data));
--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        function(error) {
+          console.error("Error getting location: " + error.message);
+        }
+      );
+    } else {
+      console.error("Geolocation is not available in this browser.");
+    }
+    
+  }, []);
+
+  useEffect(() => {
+    if (latitude !== null && longitude !== null) {
+      fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`)
+        .then(response => response.json())
+        .then(data => setData(data))
+    }
+  }, [latitude, longitude]);
+
+  
   return (
     <div className="App">
       <header className="App-header">
       <h1>FreeCode Camp <br></br>Weather App</h1>
-        
-        
-       
+      <p>{data.name}, {data.sys.country}</p>
+      <p>{data.main.temp} °C</p>
+      <p>{data.weather[0].main}</p>
+      <img src={data.weather[0].icon} alt='Wheather Icon' />
+         
+          
+
       </header>
     </div>
   );
